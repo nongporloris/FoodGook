a9b6ebd84c2f151ab9e39c9c27dd8806b31623a3
punymook <punymook@gmail.com>
2021-04-28 20:22:24 +0700

    change version of iOS

--- lib/screens/feeds.dart
+++ lib/screens/feeds.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
-
 import 'signin.dart';
 
 class Feeds extends StatefulWidget {
-   @override
+  @override
   _FeedsState createState() => _FeedsState();
- }
+}
 
- class _FeedsState extends State<Feeds> {
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
+class _FeedsState extends State<Feeds> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
         child: TextButton(
           style: TextButton.styleFrom(
-          primary:  Colors.white,
-            backgroundColor: Colors.blueAccent,
-            onSurface: Colors.grey
-          ),
-           onPressed: () {
-             Navigator.of(context)
+              primary: Colors.white,
+              backgroundColor: Colors.blueAccent,
+              onSurface: Colors.grey),
+          onPressed: () {
+            Navigator.of(context)
                 .pop(MaterialPageRoute(builder: (context) => MyHomePage()));
-           },
-           child: Text('GO TO HOME'),
-         ),
+          },
+          child: Text('GO TO HOME'),
+        ),
       ),
-     );
-   }
- }
+    );
+  }
+}
