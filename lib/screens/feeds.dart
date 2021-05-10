193813f5d384ddd1d0f9afe0008427004869144d
punymook <punymook@gmail.com>
2021-05-10 11:52:30 +0700

    Add fab speed dial

--- lib/screens/feeds.dart
+++ lib/screens/feeds.dart
@@ -1,4 +1,9 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+import 'nestedTabBarView.dart';
 // import 'package:foodgook/constant.dart';
 // import 'signin.dart';
 
@@ -7,180 +12,113 @@ class Feeds extends StatefulWidget {
   _FeedsState createState() => _FeedsState();
 }
 
-class _FeedsState extends State<Feeds> {
+class _FeedsState extends State<Feeds> with TickerProviderStateMixin {
   var selectedIndex = 0;
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 5, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     // final size = MediaQuery.of(context).size;
     Color _notiIconColor = Colors.grey;
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.receipt),
-            label: 'Recipes',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.forum),
-            label: 'forum',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_grocery_store),
-            label: 'Grocery',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
+      appBar: AppBar(
+        backgroundColor: Color(0xffff6240),
+        // title: Text("Home"),
+        // centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.notifications_none,
+            ),
+            onPressed: () {},
           ),
         ],
-        iconSize: 30,
-        currentIndex: selectedIndex,
-        onTap: (index) {
-          setState(() {
-            selectedIndex = index;
-          });
-        },
-        fixedColor: Color(0xffff6240),
-        type: BottomNavigationBarType.fixed,
       ),
-
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.symmetric(horizontal: 21.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 15.0,
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          // boxShadow: [BoxShadow(color: Colors.black45)],
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            fillColor: Color(0xfff2f2f2),
-                            filled: true,
-                            hintText: 'Search for people, recipes...',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15.0),
-                              borderSide: BorderSide.none,
-                            ),
-                            suffixIcon: Icon(Icons.search),
-                          ),
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.notifications_none),
-                      color: _notiIconColor,
-                      tooltip: 'Show all notifications',
-                      //Still can't change color when clicked
-                      onPressed: () {
-                        setState(() {
-                          if (_notiIconColor == Colors.grey) {
-                            _notiIconColor = Color(0xffff6240);
-                          } else {
-                            _notiIconColor = Colors.grey;
-                          }
-                        });
-                      },
-                      padding: const EdgeInsets.all(15.0),
-                    ),
-                  ],
-                ),
-                // ListView(
-                //   children: [
-                //     DefaultTabController(
-                //       length: 2,
-                //       initialIndex: 0,
-                //       child: Column(
-                //         children: [
-                //           TabBar(tabs: [
-                //             Tab(text: 'Feeds'),
-                //             Tab(text: 'Live show')
-                //           ]),
-                //           Container(
-                //             height: 300.0,
-                //             child: TabBarView(
-                //               children: [
-                //                 Center(child: Text('Feeds here')),
-                //                 Center(child: Text('Live show here')),
-                //               ],
-                //             ),
-                //           )
-                //         ],
-                //       ),
-                //     )
-                //   ],
-                // ),
-                // Feeds and Live show tabs
-                // SizedBox(
-                //   height: 15.0,
-                // ),
-                // Row(
-                //   crossAxisAlignment: CrossAxisAlignment.start,
-                //   children: <Widget>[
-                //     Text(
-                //       'Feeds',
-                //       style: TextStyle(
-                //         fontSize: 16,
-                //         fontWeight: FontWeight.bold,
-                //         color: Colors.grey,
-                //       ),
-                //     ),
-                //     Text(
-                //       'Live show',
-                //       style: TextStyle(
-                //         fontSize: 16,
-                //         color: Colors.grey,
-                //       ),
-                //     ),
-                //   ],
-                // ),
-              ],
-            ),
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.orange[700],
+            labelColor: Color(0xffff6240),
+            unselectedLabelColor: Colors.black54,
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.home),
+                text: 'Home',
+              ),
+              Tab(
+                icon: Icon(Icons.receipt),
+                text: 'Recipes',
+              ),
+              Tab(
+                icon: Icon(Icons.forum),
+                text: 'Forum',
+              ),
+              Tab(
+                icon: Icon(Icons.local_grocery_store),
+                text: 'Grocery',
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+                text: 'Profile',
+              ),
+            ]),
+      ),
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        backgroundColor: Colors.white,
+        foregroundColor: Color(0xffff6240),
+        overlayColor: Colors.grey,
+        overlayOpacity: 0.4,
+        curve: Curves.easeIn,
+        onOpen: () => print('Opening'),
+        onClose: () => print('Closing'),
+        children: [
+          SpeedDialChild(
+              child: Icon(Icons.add),
+              label: 'Add recipe',
+              backgroundColor: Color(0xffff6240),
+              onTap: () => print('First')),
+          SpeedDialChild(
+            child: Icon(Icons.live_tv),
+            label: 'Live show',
+            backgroundColor: Color(0xffff6240),
+            onTap: () => print('Second'),
+          )
+        ],
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          NestedTabBar(),
+          Center(
+            child: Text("Recipes"),
+            //Add page route
           ),
-        ),
+          Center(
+            child: Text("Forum"),
+          ),
+          Center(
+            child: Text("Grocery"),
+            //Add page route
+          ),
+          Center(
+            child: Text("Profile"),
+          ),
+        ],
+        controller: _tabController,
       ),
-
-      //change to image icon
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: [
-      //     BottomNavigationBarItem(
-      //       icon: ImageIcon(
-      //         AssetImage('assets/icons/home.svg'),
-      //       )
-      //       title:
-      //     ),
-      //   ],
-      // ),
     );
-
-    // return Scaffold(
-    //   body: Center(
-    //     child: TextButton(
-    //       style: TextButton.styleFrom(
-    //           primary: Colors.white,
-    //           backgroundColor: Color(0xffff6240),
-    //           onSurface: Colors.grey),
-    //       onPressed: () {
-    //         Navigator.of(context)
-    //             .pop(MaterialPageRoute(builder: (context) => MyHomePage()));
-    //       },
-    //       child: Text('GO TO HOME'),
-    //     ),
-    //   ),
-    // );
   }
 }
